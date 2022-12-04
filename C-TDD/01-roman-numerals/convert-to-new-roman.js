394925ccb368e9190da058a1326c9076822bb64a
Zaw Khing <zawmyokhing@gmail.com>
2022-12-04 12:01:17 +0000

    Changes

--- C-TDD/01-roman-numerals/convert-to-new-roman.js
+++ C-TDD/01-roman-numerals/convert-to-new-roman.js
@@ -1,5 +1,31 @@
 function convertToNewRoman(n) {
+  //create numberal to newRoman lookup table
+  //create roman numeral accumulator
+  //loop through lookup table
+  //while (currentNumber <= n) then subtract currentNumber from n, Add symbol to accumulator
 
+  const lookupTable = {
+    M: 1000,
+    D: 500,
+    C: 100,
+    L: 50,
+    X: 10,
+    V: 5,
+    I: 1,
+  };
+
+  let accumulator = '';
+
+  for(const key in lookupTable){
+     const numberValue = lookupTable[key];
+
+     while (numberValue <= n) {
+        n -= numberValue;
+        accumulator += key;
+     }
+  }
+
+  return accumulator;
 }
 
 module.exports = convertToNewRoman;
--- C-TDD/01-roman-numerals/convert-to-new-roman.test.js
+++ C-TDD/01-roman-numerals/convert-to-new-roman.test.js
@@ -4,4 +4,10 @@ test("returns I if passed 1 as an argument", function () {
   // Arrange
   // Act
   // Assert
+  let number = 1;
+  let expected = 'I';
+
+  let output = convertToNewRoman(expected);
+
+  expect(output).toBe(expected);
 });
